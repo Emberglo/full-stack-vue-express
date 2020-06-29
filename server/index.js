c60f2d2553eace1eff634771344bcafb22bb6f1d
Emberglo <rccarpenter@outlook.com>
2020-06-29 17:51:16 -0600

    setting up database and crud

---
+++ server/index.js
@@ -0,0 +1,23 @@
+//requiring dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//use express
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//set route for posts
+const posts = require('./routes/api/posts');
+
+//use the route
+app.use('/api/posts', posts);
+
+//setting port - heroku || local
+const port = process.env.PORT || 5000;
+
+//make app listen for command to start
+app.listen(port, () => console.log(`Server started on ${port}`));
---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+//require dependencies
+const express = require('express');
+const mongodb = require('mongodb');
+
+//create router
+const router = express.Router();
+
+//Get Post - just putting / references const posts in index.js
+router.get('/', async (req, res) => {
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
+});
+
+//Update Post
+
+//Add Post
+router.post('/', async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.insertOne({
+		text      : req.body.text,
+		createdAt : new Date()
+	});
+	res.status(201).send();
+});
+
+//Delete Post
+
+//connect to mongo database
+async function loadPostsCollection() {
+	const client = await mongodb.MongoClient.connect(
+		'mongodb+srv://ryan:adelaide@cluster0-gbe0j.mongodb.net/vue_express?retryWrites=true&w=majority',
+		{
+			useNewUrlParser : true
+		}
+	);
+
+	return client.db('vue_express').collection('posts');
+}
+
+//export router
+module.exports = router;
